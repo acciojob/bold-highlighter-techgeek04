eca48d4fa486fffca509ec9bb3595dfa14548d30
techgeek04 <155185231+techgeek04@users.noreply.github.com>
2024-08-31 23:13:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function highlight() {
-    //Write your code here
-
-
+    const boldWords = document.querySelectorAll('strong');
+    boldWords.forEach(word => {
+        word.style.color = 'green';
+    });
 }
 
-
 function return_normal() {
-    //Write your code here
-
-    
+    const boldWords = document.querySelectorAll('strong');
+    boldWords.forEach(word => {
+        word.style.color = 'black';
+    });
 }
